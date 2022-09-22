45247060e46dab00d345a5846446b2f9dd9199d2
Joe Heffernan <joeheffernan@Joes-MacBook-Air.local>
2022-09-21 20:40:22 -0700

    done for the night

---
+++ images/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const mongoose = require('mongoose');
+
+const PORT = 3000;
+
+const app = express();
+
+
+app.use
+
+
+
+
+
+app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
+
+module.exports = app;
--- popup.js
+++ popup.js
@@ -1,8 +1,36 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
-document.createElement
-document.body.prepend(<img src="/images/sprite.gif"></img>)
-
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
+
+// let gray = document.getElementById("c")
+// gray.addEventListener("click", setPageBackgroundColor());
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+    
+    console.log("something")
+    let sprite = document.createElement("img");
+sprite.className = "sprite"
+let file = 'images/sprite.gif';
+let url = chrome.extension.getURL(file);
+sprite.src = file;
+sprite.innerHTML = "Hello there"
+document.body.prepend(sprite)
+console.log("something")
+  });
+}
